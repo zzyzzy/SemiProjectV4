828f876c654247af1d6f42ff8643b43fbec2bcf8
zzyzigy <zzyzigy@gmail.com>
2023-05-01 16:08:11 +0900

    회원가입 - 실명확인 페이지

--- src/main/webapp/resources/js/semiprojectv4.js
+++ src/main/webapp/resources/js/semiprojectv4.js
@@ -4,6 +4,7 @@ logo.addEventListener('click', () => {
     location.href = '/';
 });
 
+// ------------------------------- agree
 // 이용약관 체크박스
 let agree1 = document.querySelector('#agree1');
 
@@ -23,3 +24,28 @@ let noagree = document.querySelector('#noagree');
 noagree?.addEventListener('click', () => {
     location.href = "/";
 });
+
+// ------------------------------- checkme
+const name2 = document.querySelector('#name2');
+const jumin1 = document.querySelector('#jumin1');
+const jumin2 = document.querySelector('#jumin2');
+const chkjumin = document.querySelector('#chkjumin');
+const chk2btn = document.querySelector('#check2btn');
+const chkfrm2 = document.querySelector('#checkfrm2');
+chk2btn?.addEventListener('click', () => {
+    if (name2.value == '') alert('이름을 입력하세요!!');
+    else if (jumin1.value == '') alert('주민번호를 입력하세요!!');
+    else if (jumin2.value == '') alert('나머지 주민번호를 입력하세요!!');
+    else if (!chkjumin.checked) alert('주민번호 처리에 동의하세요!!');
+    else {
+        chkfrm2.method = 'post';
+        chkfrm2.action = '/join/joinme';
+        chkfrm2.submit();
+    }
+});
+
+
+
+// ------------------------------- joinme
+
+// ------------------------------- joinok
