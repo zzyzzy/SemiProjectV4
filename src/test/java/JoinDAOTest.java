863d18a18b2be06fe8f90e0ad44994076244ab24
zzyzigy <zzyzigy@gmail.com>
2023-05-02 17:14:32 +0900

    회원가입 - 아이디 중복체크 RESTful 4 - dao, mybatis test

--- src/test/java/JoinDAOTest.java
+++ src/test/java/JoinDAOTest.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -17,7 +20,15 @@ public class JoinDAOTest {
     public void findZipcode() {
         String dong = "%구로%";
         assertNotNull( sqlSession.selectList("join.findZipcode", dong) );
-        System.out.println( sqlSession.selectList("join.findZipcode", dong) );
+    }
+
+    @Test
+    public void checkUserid() {
+        String uid = "abc123";   // 존재하지 않은 아이디 검사
+        assertEquals(0, (int)sqlSession.selectOne("join.selectOneUid", uid) );
+
+        uid = "abc123";          // 존재하는 아이디 검사
+        assertEquals(1, (int)sqlSession.selectOne("join.selectOneUid", uid) );
     }
 
 }
