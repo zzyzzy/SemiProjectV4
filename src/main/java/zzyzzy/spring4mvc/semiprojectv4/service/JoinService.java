9b386ba338ba36fe4a0bcc6c75ceabe5caa28f3d
zzyzigy <zzyzigy@gmail.com>
2023-05-02 11:16:46 +0900

    회원가입 - 우편번호검색 RESTful 2 - service

---
+++ src/main/java/zzyzzy/spring4mvc/semiprojectv4/service/JoinService.java
@@ -0,0 +1,15 @@
+package zzyzzy.spring4mvc.semiprojectv4.service;
+
+import zzyzzy.spring4mvc.semiprojectv4.model.Member;
+
+public interface JoinService {
+
+    String findZipcode(String dong);
+
+    boolean newMember(Member m);
+
+    boolean checkUserid(String uid);
+
+    boolean loginMember(Member m);
+
+}
