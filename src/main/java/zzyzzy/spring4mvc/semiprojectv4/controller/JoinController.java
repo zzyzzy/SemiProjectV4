925c1dfb753182d9529ffbfba94946768d41eaf7
zzyzigy <zzyzigy@gmail.com>
2023-05-02 11:15:51 +0900

    회원가입 - 우편번호검색 RESTful 1 - controller

--- src/main/java/zzyzzy/spring4mvc/semiprojectv4/controller/JoinController.java
+++ src/main/java/zzyzzy/spring4mvc/semiprojectv4/controller/JoinController.java
@@ -1,16 +1,20 @@
 package zzyzzy.spring4mvc.semiprojectv4.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import zzyzzy.spring4mvc.semiprojectv4.model.Member;
+import zzyzzy.spring4mvc.semiprojectv4.service.JoinService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/join")
 public class JoinController {
+
+    @Autowired private JoinService jnsrv;
 
     @GetMapping("/agree")
     public String agree() {
@@ -36,4 +40,22 @@ public class JoinController {
         return "join/joinok.tiles";
     }
 
+    // 우편번호 검색
+    // /join/zipcode?dong=동이름
+    // 검색결과는 뷰페이지 없이 바로 응답으로 출력 : RESTful 방식
+    // 서블릿에서 제공하는 HttpServletResponse 객체를 이용하면
+    // 스프링의 뷰리졸버 없이 바로 응답으로 출력할 수 있음
+    // 단, 응답유형은 JSON 형식으로 함
+    @ResponseBody
+    @GetMapping("/zipcode")
+    public void zipcode(String dong, HttpServletResponse res) {
+        try {
+            // 응답유형은 JSON으로 설정
+            res.setContentType("application/json; charset=utf-8");
+            // 검색된 우편번호 결과를 뷰없이 바로 응답으로 출력
+            res.getWriter().print( jnsrv.findZipcode(dong) );
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
